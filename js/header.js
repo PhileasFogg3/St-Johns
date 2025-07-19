a559560072fd3f60e159bf86275c607461aa9141
PhileasFogg3 <philip.beswick@hotmail.com>
2025-07-19 20:45:25 +0100

    Testing this

--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-fetch("../html/header.html")
+fetch("https://github.com/PhileasFogg3/St-Johns/html/header.html")
   .then((res) => res.text())
   .then((html) => {
     document.getElementById("header-placeholder").innerHTML = html;
--- js/rightSideBar.js
+++ js/rightSideBar.js
@@ -1,4 +1,4 @@
-fetch("../html/rightSideBar.html")
+fetch("https://github.com/PhileasFogg3/St-Johns/html/rightSideBar.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("sidebar-placeholder").innerHTML = html;
